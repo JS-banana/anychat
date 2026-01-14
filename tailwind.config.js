d3928003694617d69554566921796df43e5b840b
sunss <oooooohoney@163.com>
2026-01-14 22:01:55 +0800

    chore(tooling): add prettier and vitest config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -60,4 +57,4 @@ export default {
     },
   },
   plugins: [],
-}
+};
